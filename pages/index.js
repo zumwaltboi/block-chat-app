4734d1b257bb41512c4d691a1ce5d77b0b264c54
gituser6hij <user137@protonmail.com>
2023-03-23 23:54:08 +0700

    big update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
-import { getTransactions } from '../lib/eth';
-import styles from './styles.module.css';
+import { useState } from "react";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { getTransactions } from "../lib/eth";
+import styles from "./styles.module.css";
 
 export default function Home() {
-  const [address, setAddress] = useState('');
+  const [address, setAddress] = useState("");
   const [transactions, setTransactions] = useState([]);
   const [colors, setColors] = useState({});
 
@@ -24,21 +24,55 @@ export default function Home() {
 
   const getColor = (sender) => {
     if (!colors[sender]) {
-      const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      const color = `rgb(${Math.floor(Math.random() * 51)}, ${Math.floor(Math.random() * 51)}, ${Math.floor(Math.random() * 51)})`;
       setColors((prevColors) => ({ ...prevColors, [sender]: color }));
     }
-
+  
     return colors[sender];
   };
 
   return (
     <div className={styles.container}>
+     <div className={styles.header} style={{ display: 'flex', alignItems: 'center' }}>
+  <a href="https://auditutils.com/">
+    <img
+      style={{
+        maxWidth: '96px',
+        borderRadius: '15px',
+        border: '6px solid rgba(10, 202, 166, 0.9)',
+        margin: '30px',
+        padding: '15px',
+      }}
+      src="https://auditutils.com/content/images/2023/02/au-pixelize.jpg"
+      alt="auditutils logo pixel"
+    />
+  </a>
+  <h1 style={{ flex: 1, textAlign: 'center' }}>BlockChat by Audit Utils</h1>
+  <a href="https://user137-portfolio.auditutils.com">
+    <img
+      style={{
+        maxWidth: '96px',
+        borderRadius: '15px',
+        border: '6px solid rgba(10, 202, 166, 0.9)',
+        margin: '30px',
+        padding: '15px',
+      }}
+      src="https://user137-portfolio.auditutils.com/user137.PNG"
+      alt="user137 Profile Picture"
+    />
+  </a>
+</div>
+
       <form className={styles.form} onSubmit={handleSubmit}>
         <label>
           Enter Ethereum Address:
-          <input type="text" value={address} onChange={(event) => setAddress(event.target.value)} />
+          <input
+            type="text"
+            value={address}
+            onChange={(event) => setAddress(event.target.value)}
+          />
         </label>
-        <button type="submit">Fetch Transactions</button>
+        <button type="submit">Get chat history</button>
       </form>
       {transactions.length > 0 && (
         <div className={styles.conversation}>
@@ -53,28 +87,31 @@ export default function Home() {
 
               return (
                 <ListGroupItem key={transaction.hash}>
-                  <div className={styles.message} style={{ backgroundColor: color }}>
+                  <div className={styles.message} style={{ backgroundColor: color, borderRadius: '15px', border: '6px solid rgba(10, 202, 166, 0.9)', margin: '10px', padding: '10px' }}>
                     {transaction.from === address ? (
                       <div>
                         <p>
-                          <strong>You:</strong> sent a transaction to {transaction.to}
+                          <strong>target address</strong> to{" "}
+                          {transaction.to}
                         </p>
+                        <div className="spacer" />
                         <p>
-                          <strong>Input Data:</strong> {transaction.inputData}
+                          <strong>Message:</strong> {transaction.inputData}
                         </p>
                       </div>
                     ) : (
                       <div>
                         <p>
-                          <strong>{transaction.from}:</strong> sent a transaction to you
+                          <strong>{transaction.from}</strong> to target address
                         </p>
+                        <div className="spacer" />
                         <p>
-                          <strong>Input Data:</strong> {transaction.inputData}
+                          <strong>message:</strong> {transaction.inputData}
                         </p>
                       </div>
                     )}
                   </div>
-                  {index !== transactions.length - 1 && <hr className={styles.hr} />}
+                  
                 </ListGroupItem>
               );
             })}
